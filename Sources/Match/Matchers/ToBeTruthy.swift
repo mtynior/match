420c9ad16b3bae648fbc9f4d0aed6cb22ca35928
Michal Tynior <michal.git@tynior.com>
2021-10-17 19:00:52 +0200

    Add ToBeTruthy matcher

--- Sources/Match/Matchers/ToBeTruthy.swift
+++ Sources/Match/Matchers/ToBeTruthy.swift
@@ -1,24 +1,24 @@
 //
-//  ToBeFalsy.swift
+//  ToBeTruthy.swift
 //  Match
 //
 //  Created by Michal on 17/10/2021.
 //
 
-/// Tests whether the value is Falsy.
+/// Tests whether the value is Truthy.
 ///
 /// ```swift
-/// expect("Anakin".isEmpty).toBeFalsy() // Passes
-/// expect("".isEmpty).toBeFalsy() // Failes
+/// expect("Anakin".isEmpty).toBeTruthy() // Fails
+/// expect("".isEmpty).toBeTruthy() // Passes
 /// ```
 /// Note:
 ///
 /// The code above is just an example. To verify whether a collection is empty use ``Expectation/toBeEmpty(file:line:)``.
 ///
-public struct ToBeFalsy: Matcher {
+public struct ToBeTruthy: Matcher {
     public typealias ResultType = Bool
     
-    public let matcherName: String = "toBeFalsy"
+    public let matcherName: String = "toBeTruthy"
     
     public let expectation: Expectation<ResultType>
     
@@ -39,15 +39,15 @@ public struct ToBeFalsy: Matcher {
             return EvaluationResult(message: "Could not evaluate the expression", evaluationStatus: .failed, evaluationContext: evaluationContext)
         }
         
-        let isFalsy = actualValue == false
+        let isTruthy = actualValue == true
         
-        let evaluationStatus = EvaluationStatus(boolValue: isFalsy, evaluationType: expectation.evaluationType)
+        let evaluationStatus = EvaluationStatus(boolValue: isTruthy, evaluationType: expectation.evaluationType)
         let message: String = {
             switch evaluationStatus {
             case .passed:
-                return "Received\(isNegated ? " not" : "") falsy value"
+                return "Received\(isNegated ? " not" : "") truthy value"
             case .failed:
-                return "Expected\(isNegated ? " not" : "") falsy value, but received \(isNegated ? "false" : "true")"
+                return "Expected\(isNegated ? " not" : "") truthy value, but received \(isNegated ? "true" : "false")"
             }
         }()
 
@@ -61,15 +61,15 @@ public struct ToBeFalsy: Matcher {
 
 // MARK: - DSL
 public extension Expectation where ReturnType == Bool {
-    /// Verifies whether a value is falsy.
+    /// Verifies whether a value is truthy.
     ///
-    /// See the ``ToBeFalsy`` for more information.
+    /// See the ``ToBeTruthy`` for more information.
     ///
     /// - Parameters:
     ///     - file: The file where evaluation was triggered.
     ///     - line: The line number where the evaluation was triggered.
-    func toBeFalsy(file: String = #filePath, line: UInt = #line) {
-        let matcher = ToBeFalsy(expectation: self, sourceCodeLocation: SourceCodeLocation(file: file, line: line))
+    func toBeTruthy(file: String = #filePath, line: UInt = #line) {
+        let matcher = ToBeTruthy(expectation: self, sourceCodeLocation: SourceCodeLocation(file: file, line: line))
         let evaluationResult = matcher.evaluate()
         self.environment.resultReporter.reportResult(evaluationResult)
     }
---
+++ Tests/MatchTests/Matchers/ToBeTruthyTests.swift
@@ -0,0 +1,109 @@
+//
+//  ToBeTruthyTests.swift
+//  Match
+//
+//  Created by Michal on 17/10/2021.
+//
+
+import XCTest
+@testable import Match
+
+final class ToBeTruthyTests: XCTestCase {
+    var mockedReporter: MockedReporter!
+    
+    override func setUpWithError() throws {
+        mockedReporter = MockedReporter()
+        Environment.global.resultReporter = mockedReporter
+    }
+
+    override func tearDownWithError() throws {
+        mockedReporter = nil
+    }
+
+    func testPositiveEvaluation() throws {
+        XCTAssertEqual(
+            makeMatcher(actualValue: true).evaluate().evaluationStatus,
+            .passed,
+            "ToBeTruthy should be evaluated as `passed` for a truthy value"
+        )
+        
+        XCTAssertEqual(
+            makeMatcher(actualValue: false).evaluate().evaluationStatus,
+            .failed,
+            "ToBeTruthy should be evaluated as `failed` for a flasy value"
+        )
+    }
+    
+    func testNegatedEvaluation() throws {
+        XCTAssertEqual(
+            makeMatcher(actualValue: true, evaluationType: .negated).evaluate().evaluationStatus,
+            .failed,
+            "Negated ToBeTruthy should be evaluated as `failed` for a truthy value"
+        )
+        
+        XCTAssertEqual(
+            makeMatcher(actualValue: false, evaluationType: .negated).evaluate().evaluationStatus,
+            .passed,
+            "Negated ToBeTruthy should be evaluated as `passed` for a falsy value"
+        )
+    }
+    
+    func testMessages() throws {
+        // Failures
+        XCTAssertEqual(
+            makeMatcher(actualValue: false).evaluate().message,
+            "Expected truthy value, but received false"
+        )
+        
+        XCTAssertEqual(
+            makeMatcher(actualValue: true, evaluationType: .negated).evaluate().message,
+            "Expected not truthy value, but received true"
+        )
+        
+        // Successes
+        XCTAssertEqual(
+            makeMatcher(actualValue: true).evaluate().message,
+            "Received truthy value"
+        )
+        
+        XCTAssertEqual(
+            makeMatcher(actualValue: false, evaluationType: .negated).evaluate().message,
+            "Received not truthy value"
+        )
+    }
+    
+    func testMatcherName() throws {
+        // given
+        let expectedResult = "toBeTruthy"
+       
+        // when
+        let actualMatcher = makeMatcher(actualValue: true)
+        
+        // then
+        XCTAssertEqual(actualMatcher.matcherName, expectedResult)
+    }
+    
+    func testReportEvaluationResult() throws {
+        // given
+        let reportExpectation = expectation(description: "toBeTruthy() should report the evaluation result")
+        
+        mockedReporter.onReportResult = { _ in
+            reportExpectation.fulfill()
+        }
+       
+        // when
+        expect("".isEmpty).toBeFalsy()
+        
+        // then
+        waitForExpectations(timeout: 5)
+    }
+}
+
+private extension ToBeTruthyTests {
+    func makeMatcher(actualValue: Bool, evaluationType: EvaluationType = .positive) -> ToBeTruthy {
+        return ToBeTruthy(
+            expectation: MockedData.mockedExpectation(value: actualValue, evaluationType: evaluationType),
+            sourceCodeLocation: MockedData.mockedSourceCodeLocation()
+        )
+    }
+}
